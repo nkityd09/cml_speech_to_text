86eb3aced6da1d79df601bd41be655ddd143f6f2
nkityd09 <ankity@cloudera.com>
2023-10-02 14:54:57 -0700

    Updated App

--- gradio-app/app.py
+++ gradio-app/app.py
@@ -23,17 +23,21 @@ import tempfile
 import os
 import pandas as pd
 from datetime import datetime
+import time
 print(os.environ['PATH'])
+
+
+
+####################
+#Whisper
+####################
 
 MODEL_NAME = "openai/whisper-large-v2"
 BATCH_SIZE = 8
 FILE_LIMIT_MB = 1000
-
-access_token = os.environ["HF_TOKEN"]
-!huggingface-cli login --token $HF_TOKEN
-
-
+#TODO:FIX Add functionality to load in CPU if only 1 GPU available
 device = 0 if torch.cuda.is_available() else "cpu"
+#device = "cpu"
 
 pipe = pipeline(
     task="automatic-speech-recognition",
@@ -42,14 +46,19 @@ pipe = pipeline(
     device=device,
 )
 
+####################
+#Llama2
+####################
+#TODO:FIX Test with different models
+access_token = os.environ["HF_TOKEN"]
+!huggingface-cli login --token $HF_TOKEN
 
-
-## Updated Code
 hugging_face_model = "meta-llama/Llama-2-7b-chat-hf"
+#hugging_face_model = "mistralai/Mistral-7B-Instruct-v0.1"
 
 tokenizer = AutoTokenizer.from_pretrained(hugging_face_model)
 
-llm_model = AutoModelForCausalLM.from_pretrained("meta-llama/Llama-2-7b-chat-hf", #meta-llama/Llama-2-13b-chat-hf
+llm_model = AutoModelForCausalLM.from_pretrained(hugging_face_model, #meta-llama/Llama-2-13b-chat-hf
                                                      load_in_4bit=True,
                                                      device_map='balanced_low_0',
                                                      torch_dtype=torch.float16,
@@ -72,87 +81,133 @@ llm_pipeline = pipeline(
 
 text_llm = HuggingFacePipeline(pipeline=llm_pipeline)
 
-# Prompt Template for Langchain
+
+#TODO:FIX Remove excess contents from Default prompt below
+# Default Prompt Template for Langchain
 template = """You are a helpful AI assistant, please provide a detailed summary for the given context which are meeting notes. Divide your response into a Summary, Key takeaways and Action Items.
 Context:{context}
 >>Summary<<"""
 prompt_template = PromptTemplate(input_variables=["context"], template = template)
 
-
 text_chain = LLMChain(llm=text_llm, prompt=prompt_template)
 
+#####################
+#Langchain Prompts
+#####################
 
 map_prompt_template = """Write a concise summary of the following text delimited by triple backquotes.
 Return your response in bullet points which covers the key points of the text.
 ```{text}```
 BULLET POINT SUMMARY:"""
-MAP_PROMPT = PromptTemplate(template=map_prompt_template, input_variables=["text"])
-
 
 combine_prompt_template = """You will be given a series of summaries from a meeting. The summaries will be enclosed in triple backticks (```)
 Your goal is to give a detailed summary of what happened in the meeting.
 The reader should be able to grasp what happened in the meeting.
 ```{text}```
 VERBOSE SUMMARY:"""
-COMBINE_PROMPT = PromptTemplate(template=combine_prompt_template, input_variables=["text"])
 
-# summary_chain = load_summarize_chain(llm=text_llm, chain_type="map_reduce", return_intermediate_steps=True , map_prompt=MAP_PROMPT,combine_prompt=COMBINE_PROMPT,token_max=8192 ,verbose=True)
+##########################
+#Prompt Logging
+##########################
+#TODO:FIX Load Prompt Log after restart
 
 
-columns = ['Time', 'Map_Prompt', 'Combine_Prompt', 'Summary']
-logging_df = pd.DataFrame(columns=columns)
+columns = ['Time', 'Latency(s)', 'Chunk_Size', 'Chunk_Overlap', 'Map_Prompt', 'Combine_Prompt', 'Summary']
+file_path = '/home/cdsw/prompt_log/prompt_log.csv'
 
-def logging(df, datetime, map_prompt, combine_prompt, summary):
+# Check if the file exists
+if os.path.exists(file_path):
+    logging_df = pd.read_csv(file_path)
+else:
+    logging_df = pd.DataFrame(columns=columns)
+    
+def logging(df, datetime, latency, chunk_size, chunk_overlap, map_prompt, combine_prompt, summary):
     new_row = {
         'Time': datetime,
+        'Latency': latency,
+        'Chunk_Size': chunk_size, 
+        'Chunk_Overlap': chunk_overlap,
         'Map_Prompt': map_prompt,
         'Combine_Prompt': combine_prompt,
         'Summary': summary
     }
     df.loc[len(df)] = new_row
-    df.to_csv("/home/cdsw/prompt_log/prompt_log.csv")
+    df.to_csv("/home/cdsw/prompt_log/prompt_log.csv", index=False)
     return df
 
+##########################
+#Setting Up Prompt Logging
+##########################    
 
-def transcribe_summarize(inputs, task, m_prompt, c_prompt):
+
+
+def transcribe_summarize(inputs, task, m_prompt, c_prompt, chunk_size, chunk_overlap):
     if inputs is None:
         raise gr.Error("No audio file submitted! Please upload or record an audio file before submitting your request.")
-
-    text = pipe(inputs, batch_size=BATCH_SIZE, generate_kwargs={"task": task}, return_timestamps=True)["text"]
-    ##Updated Code###
-
+    text = pipe(inputs, 
+                batch_size=BATCH_SIZE, 
+                generate_kwargs={"task": task}, 
+                return_timestamps=True)["text"]
+    
     MAP_PROMPT = PromptTemplate(template=m_prompt, input_variables=["text"])
     COMBINE_PROMPT = PromptTemplate(template=c_prompt, input_variables=["text"])
-    summary_chain = load_summarize_chain(llm=text_llm, chain_type="map_reduce", return_intermediate_steps=True, map_prompt=MAP_PROMPT,combine_prompt=COMBINE_PROMPT,token_max=8192 ,verbose=True)
-    
-    text_splitter = RecursiveCharacterTextSplitter(chunk_size=4000, chunk_overlap=100)
-    
+    summary_chain = load_summarize_chain(llm=text_llm, 
+                                         chain_type="map_reduce", 
+                                         return_intermediate_steps=True, 
+                                         map_prompt=MAP_PROMPT, 
+                                         combine_prompt=COMBINE_PROMPT, 
+                                         token_max=8192, 
+                                         verbose=True)
+    text_splitter = RecursiveCharacterTextSplitter(chunk_size=int(chunk_size), chunk_overlap=int(chunk_overlap))
     docs = text_splitter.create_documents([text])
+    
+    start_time = time.time()
     output = summary_chain(docs)
+    end_time = time.time()
+    runtime = end_time - start_time
+    
     summary = output["output_text"]
-    log_df = logging(logging_df, datetime.now(), m_prompt, c_prompt, summary)
+    log_df = logging(logging_df, datetime.now(), round(runtime, 2), chunk_size, chunk_overlap, m_prompt, c_prompt, summary)
+
     return text, summary, log_df
 
 
 def transcribe(inputs, task):
     if inputs is None:
         raise gr.Error("No audio file submitted! Please upload or record an audio file before submitting your request.")
-
-    text = pipe(inputs, batch_size=BATCH_SIZE, generate_kwargs={"task": task}, return_timestamps=True)["text"]
+    text = pipe(inputs, 
+                batch_size=BATCH_SIZE, 
+                generate_kwargs={"task": task}, 
+                return_timestamps=True)["text"]
 
     return text
 
 
-def summarize(text, m_prompt, c_prompt):
+def summarize(text, m_prompt, c_prompt, chunk_size, chunk_overlap):
     MAP_PROMPT = PromptTemplate(template=m_prompt, input_variables=["text"])
     COMBINE_PROMPT = PromptTemplate(template=c_prompt, input_variables=["text"])
-    summary_chain = load_summarize_chain(llm=text_llm, chain_type="map_reduce", return_intermediate_steps=True , map_prompt=MAP_PROMPT,combine_prompt=COMBINE_PROMPT,token_max=8192 ,verbose=True)
+    summary_chain = load_summarize_chain(llm=text_llm, 
+                                         chain_type="map_reduce", 
+                                         return_intermediate_steps=True, 
+                                         map_prompt=MAP_PROMPT, 
+                                         combine_prompt=COMBINE_PROMPT, 
+                                         token_max=8192, 
+                                         verbose=True)
     text_splitter = RecursiveCharacterTextSplitter(chunk_size=4000, chunk_overlap=100)
     docs = text_splitter.create_documents([text])
+    start_time = time.time()
     output = summary_chain(docs)
+    end_time = time.time()
+    runtime = end_time - start_time
+    
     summary = output["output_text"]
-    log_df = logging(logging_df, datetime.now(), m_prompt, c_prompt, summary)
+    log_df = logging(logging_df, datetime.now(), round(runtime, 2), chunk_size, chunk_overlap, m_prompt, c_prompt, summary)
+
     return summary, log_df
+
+##########################
+#Setting Up Prompt Logging
+##########################    
 
 
 with gr.Blocks() as demo:
@@ -173,14 +228,46 @@ with gr.Blocks() as demo:
             summarized_text = gr.Textbox(label="Summarized Text", show_copy_button=True)
     with gr.Accordion("Advanced Options", open=False):
         with gr.Row():
-            map_prompt_ui = gr.Textbox(label="Map Prompt", interactive=True, show_copy_button=True, value=map_prompt_template, lines=5)
-            combine_prompt_ui = gr.Textbox(label="Combine Prompt", interactive=True, show_copy_button=True, value=combine_prompt_template, lines=5)
-    with gr.Accordion("Prompt Log", open=False):
-        log_df_ui = gr.DataFrame(wrap=True)
-        
-    transcribe_button_submit = transcribe_button.click(fn=transcribe, inputs=[audio_file, task], outputs=[transcribed_text])
-    summarize_button_submit = summary_button.click(fn=summarize, inputs=[transcribed_text, map_prompt_ui, combine_prompt_ui ], outputs=[summarized_text, log_df_ui])
-    all_button_submit = ts_button.click(fn=transcribe_summarize, inputs=[audio_file, task, map_prompt_ui, combine_prompt_ui], outputs=[transcribed_text, summarized_text, log_df_ui])
+            map_prompt_ui = gr.Textbox(label="Map Prompt", 
+                                       interactive=True, 
+                                       show_copy_button=True, 
+                                       value=map_prompt_template, 
+                                       lines=5, 
+                                       info="Use {text} to pass transcribed text")
+            combine_prompt_ui = gr.Textbox(label="Combine Prompt", 
+                                           interactive=True, 
+                                           show_copy_button=True, 
+                                           value=combine_prompt_template, 
+                                           lines=5, 
+                                           info="Use {text} to pass combined text")
+        with gr.Row():
+            chunk_size = gr.Textbox(label="Chunk Size", show_copy_button=True, interactive=True, value=4000)
+            chunk_overlap = gr.Textbox(label="Chunk Overlap", show_copy_button=True, interactive=True, value=100)
+        with gr.Accordion("Prompt Log", open=False):
+            with gr.Row():    
+                log_df_ui = gr.DataFrame(wrap=True, height=5)
+ 
+    transcribe_button_submit = transcribe_button.click(fn=transcribe, 
+                                                       inputs=[audio_file, task], 
+                                                       outputs=[transcribed_text])
+    summarize_button_submit = summary_button.click(fn=summarize, 
+                                                   inputs=[transcribed_text, 
+                                                           map_prompt_ui, 
+                                                           combine_prompt_ui, 
+                                                           chunk_size, 
+                                                           chunk_overlap ], 
+                                                   outputs=[summarized_text, 
+                                                            log_df_ui])
+    all_button_submit = ts_button.click(fn=transcribe_summarize, 
+                                        inputs=[audio_file, 
+                                                task, 
+                                                map_prompt_ui, 
+                                                combine_prompt_ui, 
+                                                chunk_size, 
+                                                chunk_overlap], 
+                                        outputs=[transcribed_text, 
+                                                 summarized_text, 
+                                                 log_df_ui])
 
 if __name__ == "__main__":
     demo.launch(share=True,
